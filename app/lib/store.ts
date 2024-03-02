2c9b9eb5942b8e8a542c813d4ce5585c8a7dd748
KimDung29 <kim929vn@gmail.com>
2024-03-02 12:50:39 +0700

    redux

---
+++ app/lib/store.ts
@@ -0,0 +1,23 @@
+// store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+import authReducer from '@/app/lib/features/auth/authSlice'
+// Define a RootState type
+export type RootState = ReturnType<typeof store.getState>;
+
+// Create the Redux store
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+// Export the store
+export default store;
+
+// Export a custom useDispatch hook with correct type
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+// Export a custom useSelector hook with correct type
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
